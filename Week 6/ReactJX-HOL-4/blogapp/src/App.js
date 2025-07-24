2076414195c9394cb4cda8402ead5e4ee503a741
SaranSelva18 <git.with.saran@gmail.com>
2025-07-24 18:58:20 +0530

    Created blogapp React app and implement Posts class component to fetch and display posts

---
+++ Week 6/ReactJX-HOL-4/blogapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from "react";
+import Posts from "./Posts/Posts";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Posts Viewer</h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
