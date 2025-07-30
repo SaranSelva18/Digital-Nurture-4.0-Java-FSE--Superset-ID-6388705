e92af45ce2a4c97f9c472605763f249546fae97f
SaranSelva18 <git.with.saran@gmail.com>
2025-07-30 14:52:35 +0530

    Used Constructor and state to Store the entrycount and exitcount.

---
+++ Week 6/ReactJX-HOL-8/counterapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CountPeople from './CountPeople';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <CountPeople />
+    </div>
+  );
+}
+
+export default App;
