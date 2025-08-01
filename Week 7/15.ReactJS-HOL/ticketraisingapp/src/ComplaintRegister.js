b000ad90c780fb9cdb440fed70222bb87f766c52
SaranSelva18 <git.with.saran@gmail.com>
2025-08-01 21:39:55 +0530

    Created ComplaintRegister form with aligned layout and alert-based complaint confirmation

---
+++ Week 7/15.ReactJS-HOL/ticketraisingapp/src/ComplaintRegister.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class ComplaintRegister extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ename: '',
+      complaint: '',
+      NumberHolder: Math.floor(Math.random() * 1000000) + 1
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    const msg = `Thanks ${this.state.ename} \nYour Complaint was Submitted.\nTracking ID is: ${this.state.NumberHolder}`;
+    alert(msg);
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h2 style={{ color: 'red' }}>Register your complaints here!!!</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name: &nbsp;
+            <input
+              type="text"
+              name="ename"
+              value={this.state.ename}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br /><br />
+          <label>
+            Complaint: &nbsp;
+            <textarea
+              name="complaint"
+              value={this.state.complaint}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br /><br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ComplaintRegister;
